4c5128298f6ac1a30e36edac87fbb03446143c7d
cliffgor <clierozigy@gmail.com>
2024-10-07 14:12:53 +0300

    Refactor server.js to log request URL and method

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-type', 'text/html')
     // res.statusCode = 404
 
+    console.log(req.url)
+    console.log(req.method)
+
     res.writeHead(200, { 'Content-type': 'text/html' })
     res.end('<h1>Hello world!</h1>')
 })
