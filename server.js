a2374972617fb40aa6a947026db2b3cede4d8f3f
cliffgor <clierozigy@gmail.com>
2024-10-07 13:26:30 +0300

    Refactor server.js to set response headers and status code

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import http from 'http'
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-type', 'text/html')
-    res.statusCode = 404
-    res.end('<h1>Hello World!</h1>')
+    // res.setHeader('Content-type', 'text/html')
+    // res.statusCode = 404
+
+    res.writeHead(500, {'Content-type': 'application/json'})
+    res.end(JSON.stringify({message: 'Server Error'}))
 })
 
 server.listen(PORT, () => {
