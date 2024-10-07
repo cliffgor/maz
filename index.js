b6159126ce5870e6b1060db926d05726159c7b25
cliffgor <clierozigy@gmail.com>
2024-10-07 12:18:06 +0300

    working with es modules

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { generateRandomNumber, celciusToFerenheit } = require("./utils")
+import { getPosts } from "./postController.js"
 
-console.log(`Random Number: ${generateRandomNumber()}`)
-console.log(`Celcius :${celciusToFerenheit(0)}`)
+// const { generateRandomNumber, celciusToFerenheit } = require("./utils")
+
+// console.log(`Random Number: ${generateRandomNumber()}`)
+// console.log(`Celcius :${celciusToFerenheit(0)}`)
+
+
+console.log(getPosts());
---
+++ postController.js
@@ -0,0 +1,10 @@
+const posts = [
+    { id: 1, title: "Learning 1" },
+    { id: 2, title: "Learning 2" },
+    { id: 3, title: "Learning 3" },
+    { id: 4, title: "Learning 4" },
+    { id: 5, title: "Learning 5" },
+    { id: 6, title: "Learning 6" },
+]
+
+export const getPosts = () => posts
