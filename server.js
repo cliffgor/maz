3f4377b5d4144899709ae7b4321a7ae2dd35d938
cliffgor <clierozigy@gmail.com>
2024-10-08 11:32:12 +0300

    Refactor server.js to use fs/promises for file reading Add public/index.html and public/about.html

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
+import fs from 'fs/promises'
 import http from 'http'
+import path from 'path'
+import url from 'url'
 
 const PORT = process.env.PORT
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        res.end('<h1>Home page</h1>')
-    } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        res.end('<h1>About page</h1>')
-    } else {
-        res.writeHead(404, { 'Content-type': 'text/html' })
-        res.end('<h1>Not Found</h1>')
+console.log(__filename,__dirname);
+
+
+const server = http.createServer(async(req, res) => {
+
+    try {
+        // Check if get request
+        if (req.method === 'GET') {
+            let filePath
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html')
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html')
+            } else {
+                throw new Error('Not Found')
+            }
+
+            const data = await fs.readFile(filePath)
+            res.setHeader('Content-type', 'text/html')
+            res.write(data)
+            res.end()
+        } else {
+            throw new Error('Method not allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-type': 'text/plain' })
+        res.end('Server Error')
     }
+
+
+
+
 
 
 
